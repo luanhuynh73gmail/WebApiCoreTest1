9bf364015910ebcf9145f74adac5944273e4b0c5
LuanHuynh <68029128+luanhuynh73gmail@users.noreply.github.com>
2026-07-16 19:21:13 -0500

    check  in 1 branch 1

--- WebapiCoreTest1/WebapiCoreTest1/WeatherForecast.cs
+++ WebapiCoreTest1/WebapiCoreTest1/WeatherForecast.cs
@@ -2,6 +2,7 @@ namespace WebapiCoreTest1
 {
     public class WeatherForecast
     {
+        //branch1 test1
         public DateOnly Date { get; set; }
 
         public int TemperatureC { get; set; }
